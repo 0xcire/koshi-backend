e12c80f758edb16dd84c57e509fe68d16b7ab273
0xcire <ciredeveloper@gmail.com>
2025-03-25 16:41:09 -0400

    basic swagger and pino logging set up

--- koshi-api/src/main.ts
+++ koshi-api/src/main.ts
@@ -1,11 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { Logger } from 'nestjs-pino';
+import { type INestApplication } from '@nestjs/common';
+
+function setUpSwaggerUI(app: INestApplication<any>) {
+  const config = new DocumentBuilder()
+    .setTitle('Koshi API')
+    .setDescription('things that you can can do')
+    .setVersion('1.0')
+    .addTag('koshi')
+    .build()
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('swagger-ui', app, documentFactory)
+}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      // origin: "https://koshi.cire.sh",
+    },
+    bodyParser: true,
+    bufferLogs: true
+  });
+  app.useLogger(app.get(Logger))
+
+  setUpSwaggerUI(app)
+
   await app.listen(1338);
   console.log("listening on 1338")
-
-
 }
+
 bootstrap();
