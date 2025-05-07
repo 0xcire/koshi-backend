fc10f4d7c50206aa64a2612d5fae6922dc6e55ef
0xcire <ciredeveloper@gmail.com>
2025-05-07 19:44:54 -0400

    misc

--- koshi-api/src/types.ts
+++ koshi-api/src/types.ts
@@ -1,3 +1,9 @@
-export enum ENV {
-  KOSHI_PG_URL = 'KOSHI_PG_URL'
+export enum NodeEnvironment {
+  Prod = 'production',
+  Dev = 'development',
 }
+
+export const ONE_HOUR_AS_SECONDS = 3600;
+export const FIVE_MIN_AS_SECONDS = 300;
+
+export const IS_PROD = process.env.NODE_ENV === NodeEnvironment.Prod;
