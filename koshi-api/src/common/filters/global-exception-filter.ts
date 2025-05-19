8d24a72fffc490a7f090a7061decaf97bb0ab043
0xcire <ciredeveloper@gmail.com>
2025-05-18 23:14:38 -0400

    auth guard

---
+++ koshi-api/src/common/filters/global-exception-filter.ts
@@ -0,0 +1,6 @@
+// import { ArgumentsHost, ExceptionFilter } from '@nestjs/common';
+// import { BaseExceptionFilter } from '@nestjs/core';
+
+// export class GlobalExceptionFilter implements ExceptionFilter {
+//   catch(exception: any, host: ArgumentsHost) {}
+// }
